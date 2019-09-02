be41f64b4f8ae8994c1fbc842c7ba52b50003702
hyc449 <hyc449@nyu.edu>
2019-09-02 16:07:39 -0600

    Initialization with required files

---
+++ spell.c
@@ -0,0 +1,4 @@
+int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]);
+bool load_dictionary(const char* dictionary_file, hashmap_t
+hashtable[]);
+bool check_word(const char* word, hashmap_t hashtable[]);
